9d39a95736149cb67ac3449b6749f3fe1c985525
Hritikraj8804 <hritikraj8804@gmail.com>
2025-06-28 19:22:52 +0530

    project initialization

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+
+        // Define a simple route
+        app.get("/", (req, res) => {
+            res.send("Welcome to the Ticket Management API");
+        });
+
+        // Start the server
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => console.error("MongoDB connection error:", err));
---
+++ models/ticket.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { assign } from 'nodemailer/lib/shared';
+
+const ticketSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    status: { type: String, default: "TODO" },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    assignedTo: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User' ,
+        default: null
+    },
+    priority: String,
+    deadline: Date,
+    helpfulNotes: ServerTiming,
+    relatedSkills: [String],
+    createdAt: { type: Date, default: Date.now },
+})
+
+export default mongoose.model("Ticket", userSchema);
